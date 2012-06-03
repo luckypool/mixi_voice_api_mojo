2491eb0b034612933e861eb74921d151d30cc650
Yuta YOSHIIKE <luckypool314@gmail.com>
2012-06-03 11:08:49 +0900

    simple use case for mixi Graph API

--- hello.pl
+++ hello.pl
@@ -5,21 +5,52 @@ use Mojolicious::Lite;
 plugin 'PODRenderer';
 
 get '/' => sub {
-  my $self = shift;
-  $self->render('index');
+    my $self = shift;
+    unless (exists $self->session->{'access_token'}) {
+        $self->render('index');
+    }
+    $self->render('logined');
+};
+
+get '/auth' => sub {
+    my $self = shift;
+    my $url = Mojo::URL->new('https://mixi.jp/connect_authorize.pl');
+    $url->query([
+            client_id => 'XXXXXXXXXX',
+            response_type => 'code',
+            scope => 'r_voice'
+        ]);
+    $self->redirect_to($url->to_abs);
+};
+
+get '/redirect' => sub{
+    my $self = shift;
+    my $url_for_get_token = Mojo::URL->new('https://secure.mixi-platform.com/2/token');
+    $url_for_get_token->query([
+            grant_type => 'authorization_code',
+            client_id => 'XXXXXXXXXX',
+            client_secret => 'XXXXXXXXXX',
+            code => $self->param('code'),
+            redirect_uri => 'http://localhost:3000/redirect'
+        ]);
+    my $ua = Mojo::UserAgent->new;
+    my $res = $ua->post(
+        $url_for_get_token => {
+            'Content-Type' => 'application/x-www-form-urlencoded'
+        }
+    )->res->content->asset->slurp;
+    my $token_json = Mojo::JSON->decode($res);
+    $self->session(access_token => $token_json->{'access_token'});
+    $self->session(refresh_token => $token_json->{'refresh_token'});
+    $self->stash(url => '/');
+    $self->render('redirect');
+    $self->redirect_to('/');
 };
 
 app->start;
+
 __DATA__
+@@ redirect.html.ep
+% layout 'default',  title => 'redirect';
+redirect to <%= $url %>
 
-@@ index.html.ep
-% layout 'default';
-% title 'Welcome';
-Welcome to the Mojolicious real-time web framework!
-
-@@ layouts/default.html.ep
-<!DOCTYPE html>
-<html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
-</html>
